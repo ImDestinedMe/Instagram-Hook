d44bc40d27169870ff971c7860e0380c170c5e5d
Destiny Bonavita <destiny@fltplan.com>
2015-06-22 19:13:38 -0400

    Initial Commit

---
+++ Instagram Hook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Instagram Hook
+//
+//  Created by Destiny Bonavita on 6/22/15.
+//  Copyright (c) 2015 Fading Eclipse LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Instagram Hook/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Instagram Hook
+//
+//  Created by Destiny Bonavita on 6/22/15.
+//  Copyright (c) 2015 Fading Eclipse LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
